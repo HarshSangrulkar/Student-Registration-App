2020ac93f366af7f8874137a2e78526cd0a4dd9f
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2025-03-03 00:30:19 +0530

    project setup

---
+++ components/StudentDataDisplay.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+const StudentDataDisplay = () => {
+    return (
+        <View>
+            <Text>Student Data</Text>
+        </View>
+    )
+}
+
+export default StudentDataDisplay;
---
+++ components/StudentDataInput.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const StudentDataInput = () => {
+    return (
+        <View>
+            <Text>Student Registration</Text>
+        </View>
+    )
+}
+
+export default StudentDataInput;
